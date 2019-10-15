420ec8ecc0e45db3843a2446ab1b532f2dc960e2
Jakob Stasilowicz <jakob.stasilowicz@gmail.com>
2019-10-15 20:21:06 +0200

    Split sdf test scenes into separate files

--- src/store/index.js
+++ src/store/index.js
@@ -35,9 +35,9 @@ const defaultMaxSampleCount = 10;
 // const defaultSceneUrl = 'assets/scenes/model-test-scene/index.js.rtr';
 // const defaultSceneUrl = 'assets/scenes/material-test-scene/index.js.rtr';
 
-// const defaultSceneUrl = 'assets/scenes/hollow-box-sdf-scene/index.js.rtr';
-
-const defaultSceneUrl = 'assets/scenes/sdf-test-scene/index.js.rtr';
+// const defaultSceneUrl = 'assets/scenes/sdf-test-scene-1/index.js.rtr';
+const defaultSceneUrl = 'assets/scenes/sdf-test-scene-2/index.js.rtr';
+// const defaultSceneUrl = 'assets/scenes/sdf-test-scene-3/index.js.rtr';
 
 let instance = null;
 class Store {
